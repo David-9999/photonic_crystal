aa8db8a3921723ea3baee5fd816604ea75f018ef
David-9999 <52111028+David-9999@users.noreply.github.com>
2022-12-13 20:09:11 +0100

    Update intensity_transmission.py

--- intensity_transmission.py
+++ intensity_transmission.py
@@ -8,7 +8,9 @@ from numpy.linalg import multi_dot
 from numpy.linalg import matrix_power
 
 ## DAVID VRBA
-## vypocet E
+## Calculation of intensity of electric field
+
+
 epsilo_a = 1#+1j*0.00000001
 mu_a = 1#+1j*0.00000001
 
@@ -18,7 +20,7 @@ mu_b = 1+1j*0.00000001
 n_a = np.sqrt(complex(epsilo_a*mu_a))
 n_b = np.sqrt(complex(epsilo_b*mu_b))
 
-theta_a = 0 ## kolmy dopad
+theta_a = 0 
 theta_b = np.arcsin(np.sin(theta_a)*n_a.real/n_b.real)
 
 print(n_a)
@@ -34,47 +36,49 @@ l = l_a + l_b
 
 omega_0 = 2*np.pi/l
 
-N = 10 ## pocet vrstiev
+N = 10 ## lenght of A+B layers before defect 
 
-koef = 1 ## koeficient nasobku poruhcy a->koef*a
+koef = 1 ## size of defect a->koef*a
 omega = 3*omega_0/8
 
-    ## vypocet k-cok
+## Calculation of wave vectors
 k_a = omega*n_a*np.cos(theta_a)
 k_b = omega*n_b*np.cos(theta_b)
     
 chi = (mu_b*k_a)/(mu_a*k_b)
 
-    ## matica prechodu A->B
+## Trasnfer matrix A->B
 M_ab = 1/2*np.array([[1 + chi, 1 - chi], [1 - chi , 1 + chi]])
 
-    ## matica prechodu B->A
+## Trasnfer matrix B->A
 M_ba = inv(M_ab)
 
 
 
-E_plus = 1.
+## Amplidute of electric field 
+E_plus = 1. 
 E_minus = 0.
+
 x = []
-Intezita = []
+Intensity = []
 
 E = [E_plus, E_minus]
-Intezita.append(np.absolute(E[1]+E[0])**2)
+Intensity.append(np.absolute(E[1]+E[0])**2)
 
 total_lenght = 2*N*l+koef*l_a
 number_of_steps = 100000
-delta = total_lenght/number_of_steps ## dlzka kroku
+lenght_of_step = total_lenght/number_of_steps ## dlzka kroku
 
-s_a = int(l_a/delta) ## pocet krokov potrebnych na prejdenie a
-s_b = int(l_b/delta) ## pocet krokov potrebnych na prejdenie b
+s_a = int(l_a/lenght_of_step) ## Number of step needed to go through layer A
+s_b = int(l_b/lenght_of_step) ## Number of step needed to go through layer B
 
-s = int(s_a + s_b) ## pocet krokov potrebnych pre a+b
+s = int(s_a + s_b) ## Number of step needed to go through layer A+B
 
-l1 = (s_a+s_b)*N ## pocet krokov k poruche
-l2 = (s_a+s_b)*N+koef*s_a ## pocet krokov za poruchu
+l1 = (s_a+s_b)*N ## Number of step to the defect
+l2 = (s_a+s_b)*N+koef*s_a ## Number of step to and through defect 
 
-def prechod(posunutie,K):
-    return np.array([[np.exp(1j*K*posunutie), 0], [0, np.exp(-1j*K*posunutie)]])
+def transmission(step,K):
+    return np.array([[np.exp(1j*K*step), 0], [0, np.exp(-1j*K*step)]])
 print("s: ", end ="")
 print(s)
 print("s_a: ", end ="")
@@ -82,52 +86,42 @@ print(s_a)
 print("s_b: ", end ="")
 print(s_b)
 
-    # logika vypoctu je takato:
-    # viem kolko krokov urobim v prostredi A a B
-    # takze viem kolko krokov urobim cez A+B
-    # podla toho aky mam zvyskok po deleni poctu krokov cez A+B
-    # viem urcite ci ide o prostredie A, rozhranie alebo B
-    # ak zvysok i/s je mensi ako s_a som v A
-    # ak sa rovna tak som na rozhrani AB a podobne pre ostatne limity
-    # Potom si vypocitam kolko krokov l1 spravim dokym sa dostanem k poruche 
-    # aplikujem tieto podmienky 
-    # vynasobim maticu E z lava prechodovou maticou s l rovnce i*delta
-    # podobne to spravim aj ked som v poruche a za poruchou
+
 i = 1
 while i <= number_of_steps:
-    if (i <= l1): ## k poruche
+    if (i <= l1): ## To the defect
         if 0 < (i % s) < s_a:
-            E = prechod(delta,k_a).dot(E) ## dot product
+            E = transmission(lenght_of_step,k_a).dot(E) ## Dot product
         elif (i % s) == s_a:
             E = M_ab.dot(E)
         elif s_a < (i % s):
-            E = np.dot(prechod(delta,k_b),E)
+            E = np.dot(transmission(lenght_of_step,k_b),E)
         elif (i % s) == 0:
             E = M_ba.dot(E)
         else:
-            print("problem")
-    elif ((l1 < i) and (i <= l2)): ## porucha
-        E = prechod(delta,k_a).dot(E)
-    elif (l2 < i): ## od poruchy
+            print("Error!")
+    elif ((l1 < i) and (i <= l2)): ## Defect
+        E = transmission(lenght_of_step,k_a).dot(E)
+    elif (l2 < i): ## After defect
         if ((i-koef*s_a) % s) < s_a:
-            E = prechod(delta,k_a).dot(E)
+            E = transmission(lenght_of_step,k_a).dot(E)
         elif ((i-koef*s_a) % s) == s_a:
             E = M_ab.dot(E)
         elif s_a < ((i-koef*s_a) % s):
-            E = np.dot(prechod(delta,k_b),E)
+            E = np.dot(transmission(lenght_of_step,k_b),E)
         elif ((i-koef*s_a) % s) == int(s-1):
             E = M_ba.dot(E)
         else:
-            print("problem")
+            print("Error!")
     else:
-        print("problem")
+        print("Error!")
 
-    x.append(delta*(i-1)/total_lenght)
+    x.append(lenght_of_step*(i-1)/total_lenght)
     I = np.absolute(E[1]+E[0])**2
-    Intezita.append(I)
+    Intensity.append(I)
     i+=1
 
-Intezita.pop()
+Intensity.pop()
 # PLOT
 sz = 20
 lw = 0.75
@@ -159,6 +153,7 @@ ax.xaxis.set_label_position('top')
 
 plt.savefig("plot_E.png", dpi = 600, format = 'png')
 plt.close()
+
 
 
 ## vypocet transmisie
